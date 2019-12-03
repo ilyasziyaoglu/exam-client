9497cf9fbb2e9e8bf5549268b318b9a5df84ba32
ILYAS ZIYAOGLU <ilyas.ziyaoglu@consultant.turkcell.com>
2019-12-03 22:10:55 +0300

    some commit

---
+++ src/app/pages/student/student-exam-detail/student-exam-detail.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'ngx-student-exam-detail',
+  templateUrl: './student-exam-detail.component.html',
+  styleUrls: ['./student-exam-detail.component.scss'],
+})
+export class StudentExamDetailComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
